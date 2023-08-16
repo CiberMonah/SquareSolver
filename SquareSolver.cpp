ac509e2ef32251480bab584ca152356bad013dd7
CiberMonah <142305833+CiberMonah@users.noreply.github.com>
2023-08-16 16:37:00 +0300

    Update SquareSolver.cpp

--- SquareSolver.cpp
+++ SquareSolver.cpp
@@ -5,7 +5,7 @@
 
 void SquareSol(float a, float b, float c);
 
-int main (void){
+int main (void) {
     float a, b, c;
 
     printf("Enter in a row, separated by spaces, the coefficients of the quadratic equation of the form: ax^2 + bx + c = 0\n");
@@ -15,17 +15,17 @@ int main (void){
     return 0;
 }
 
-void SquareSol(float a, float b, float c){
+void SquareSol(float a, float b, float c) {
     float D = float(pow(b, 2)) - 4 * a * c;
-    
-    if (fabs(a - 0) < eps){
-        if (fabs(b - 0) < eps) printf( "infinite number of solutions" );
+
+    if (fabs(a - 0) < eps) {
+        if (fabs(b - 0) < eps) printf("infinite number of solutions");
         else printf("Sol1 = %.5f", -c / b);
     }
-    else{
+    else {
         if (fabs(D - 0) < eps)
             printf("Sol1 = %.5f\n", -b / (2 * a));
-        else if ( D > 0 ){
+        else if (fabs(D - 0) < eps) {
             printf("Sol1 = %.5f\n", ((-b  + sqrt(D)) / (2 * a)));
             printf("Sol2 = %.5f\n", ((-b  - sqrt(D)) / (2 * a)));
         }
