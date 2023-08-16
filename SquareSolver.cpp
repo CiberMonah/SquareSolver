b69a2a9d3f314648d443b80f6f09dd1bf38b4d20
CiberMonah <142305833+CiberMonah@users.noreply.github.com>
2023-08-16 15:07:36 +0300

    Update SquareSolver.cpp
    Added: 
    1. variation when a = 0
    2. Warnings were fixed

--- SquareSolver.cpp
+++ SquareSolver.cpp
@@ -1,27 +1,37 @@
 #include <stdio.h>
 #include <math.h>
 #include <TXLib.h>
+#define eps 0.00001
 
-void SquareSol(float a, float b, float c)
-{
-    float D = float(pow(b, 2)) - 4 * a * c;
-    if ( D == 0 )
-        printf("Sol1 = %.5f\n", -b / (2 * a) );
-    else if ( D > 0 )
-    {
-        printf("Sol1 = %.5f\n", ((-b  + sqrt(D))/ (2 * a)));
-        printf("Sol2 = %.5f\n", ((-b  - sqrt(D)) / (2 * a)));
-    }
-    else printf("There is no solutions\n");
-}
-
+void SquareSol(float a, float b, float c);
 
 int main (void){
     float a, b, c;
 
     printf("Enter in a row, separated by spaces, the coefficients of the quadratic equation of the form: ax^2 + bx + c = 0\n");
     scanf("%f %f %f", &a, &b, &c);
-    SquareSol(a, b , c);
+    SquareSol(a, b ,c);
 
     return 0;
+}
+
+void SquareSol(float a, float b, float c)
+{
+    float D = float(pow(b, 2)) - 4 * a * c;
+    if ( fabs(a - 0) < eps )
+    {
+        if ( fabs(b - 0) < eps ) printf("infinite number of solutions");
+        else printf("Sol1 = %.5f", -c / b);
+    }
+    else
+    {
+        if ( fabs(D - 0) < eps )
+            printf("Sol1 = %.5f\n", -b / (2 * a) );
+        else if ( D > 0 )
+        {
+            printf("Sol1 = %.5f\n", ((-b  + sqrt(D))/ (2 * a)));
+            printf("Sol2 = %.5f\n", ((-b  - sqrt(D)) / (2 * a)));
+        }
+        else printf("There is no solutions\n");
+    }
 }
