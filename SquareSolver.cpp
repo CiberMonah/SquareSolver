f2d69191e5b51ded05b0391684861780d3541884
CiberMonah <142305833+CiberMonah@users.noreply.github.com>
2023-08-16 21:24:24 +0300

    Update SquareSolver.cpp

--- SquareSolver.cpp
+++ SquareSolver.cpp
@@ -4,38 +4,59 @@
 #define eps 0.00001
 
 void SquareSol(float a, float b, float c);
+void greeting(void);
+bool check(void);
+void input(float *a, float *b, float *c);
 
-int main (void) {
+int main(void) {
     float a, b, c;
     int ch;
 
      do {
-        printf("Enter in a row, separated by spaces, the coefficients of the quadratic equation of the form: ax^2 + bx + c = 0\n");
-        scanf("%f%f%f", &a, &b, &c);
-        if (getchar()!= '\n') {
-        printf("Please, follow instructions\n");
-        return 0;
-        }
-        else {
-            SquareSol(a, b, c);
-            printf("Press esc to stop or any button to continue\n");
-            ch = _getch();
-        }
-    }while (ch!= 27);
+        greeting();
+        input(&a, &b, &c);
+        if (check()) {
+                SquareSol(a, b, c);
+                printf("Press esc to stop or any button to continue\n");
+                ch = _getch();
+         }
+        else return 0;
 
+    }while (ch!= 27);
+}
+
+void greeting(void) {
+    printf("Enter in a row, separated by spaces, the coefficients of the quadratic equation of the form: ax^2 + bx + c = 0\n");
+}
+
+bool check(void) {
+    if (getchar()!= '\n') {
+    printf("Please, follow instructions\n");
+    return false;
+    }
+    else {
+        return true;
+    }
+}
+
+void input(float *a, float *b, float *c) {
+    while (scanf("%f%f%f", a, b, c) != 3) {
+            while(getchar()!='\n')
+                ;
+            }
 }
 
 void SquareSol(float a, float b, float c) {
     float D = float(pow(b, 2)) - 4 * a * c;
 
     if (fabs(a - 0) < eps) {
-        if (fabs(b - 0) < eps) printf("infinite number of solutions");
+        if (fabs(b - 0) < eps) printf("infinite number of solutions\n");
         else printf("Sol1 = %.5f", -c / b);
     }
     else {
         if (fabs(D - 0) < eps)
             printf("Sol1 = %.5f\n", -b / (2 * a));
-        else if (fabs(D - eps) > 0) {
+        else if (D - eps > 0) {
             printf("Sol1 = %.5f\n", ((-b  + sqrt(D)) / (2 * a)));
             printf("Sol2 = %.5f\n", ((-b  - sqrt(D)) / (2 * a)));
         }
