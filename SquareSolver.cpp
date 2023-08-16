6ea022805057a383339723714aa4bfee3c771d62
CiberMonah <142305833+CiberMonah@users.noreply.github.com>
2023-08-17 00:47:36 +0300

    Update SquareSolver.cpp

--- SquareSolver.cpp
+++ SquareSolver.cpp
@@ -1,14 +1,15 @@
 #include <stdio.h>
 #include <math.h>
 #include <TXLib.h>
-#define eps 0.00001
+#define eps 0.000001
 
-void calculate_solutions(float a, float b, float c);
-void print_solutions(float solution1);
-void print_solutions(float solution1, float solution2);
+float *calculate_solutions(float a, float b, float c);
+void print_solutions(float solutions[]);
 void greeting(void);
 bool check_input(void);
 void input_coeficients(float *a, float *b, float *c);
+
+float solutions[3] = {0, 0, 0};//solutions[quantity, 1_solution, 2_solution]
 
 int main(void) {
     float a, b, c;
@@ -18,7 +19,7 @@ int main(void) {
         greeting();
         input_coeficients(&a, &b, &c);//in order
         if (check_input()) {
-                calculate_solutions(a, b, c);
+                print_solutions(calculate_solutions(a, b, c));
                 printf("Press esc to stop or any button to continue\n");
                 ch = _getch();
          }
@@ -48,28 +49,32 @@ void input_coeficients(float *a, float *b, float *c) {
         }
 }
 
-void calculate_solutions(float a, float b, float c) {
+float *calculate_solutions(float a, float b, float c) {
     float D = float(pow(b, 2)) - 4 * a * c;
 
     if (fabs(a) < eps) {
-        if (fabs(b) < eps) printf("infinite number of solutions\n");
-        else print_solutions(-c / b);
+        if (fabs(b) < eps) solutions[0] = 3;
+        else {
+            solutions[0] = 1;
+            solutions[1] = -c / b;
+        }
     }
     else {
-        if (fabs(D) < eps)
-            print_solutions(-b / (2 * a));
-        else if (D - eps > 0) {
-            print_solutions((-b  + float(sqrt(D))) / (2 * a), (-b  - float(sqrt(D))) / (2 * a));
+        if (fabs(D) < eps){
+            solutions[0] = 1;
+            solutions[1] = -b / (2 * a);
+            }
+        if  (D - eps > 0) {
+            solutions[0] = 2;
+            solutions[1] = (-b  + float(sqrt(D))) / (2 * a);
+            solutions[2] = (-b  - float(sqrt(D))) / (2 * a);
         }
-        else printf("There is no solutions\n");
     }
+    return solutions;
 }
 
-void print_solutions(float solution1, float solution2) {
-    printf("Solution_1 = %.5f\n", solution1);
-    printf("Solution_2 = %.5f\n", solution2);
-}
-
-void print_solutions(float solution1) {
-    printf("Solution_1 = %.5f\n", solution1);
+void print_solutions(float *sol) {
+    if(fabs(sol[0] - 3) <= FLT_EPSILON) printf("infinity quantity of solutions\n");
+    if(fabs(sol[0]) <= FLT_EPSILON) printf("No solutions\n");
+    for(int i = 1;i < int(sol[0]) + 1; i++) printf("Solution%d = %f\n", i, sol[i]);
 }
