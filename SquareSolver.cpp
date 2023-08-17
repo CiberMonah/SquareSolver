dc5bb4618d910410673124f47b06c34dae94d4c2
CiberMonah <142305833+CiberMonah@users.noreply.github.com>
2023-08-17 20:52:49 +0300

    Update SquareSolver.cpp

--- SquareSolver.cpp
+++ SquareSolver.cpp
@@ -11,28 +11,28 @@ struct Coeficients {
     float a, b, c;
 };
 
-struct Solutions *calculate_solutions(struct Coeficients coeficients);
-void print_solutions(struct Solutions *solutions);
+void calculate_solutions(struct Coeficients coeficients, struct Solutions *solutions);
+void print_solutions(struct Solutions solutions);
 void greeting(void);
 bool check_input(void);
-void input_coeficients(float *a, float *b, float *c);
+bool check_float_equality(float f_1, float f_2);
+void input_coeficients(struct Coeficients *coeficients);
 
 const int esc_button = 27;
 const float EPSILON = float(0.00001);
 
 int main(void) {
     struct Coeficients coeficients;
+    struct Solutions solutions;
     int ch;
 
     do {
         greeting();
-        input_coeficients(&coeficients.a, &coeficients.b, &coeficients.c);//in order
-        if (check_input()) {
-            print_solutions(calculate_solutions(coeficients));
-            printf("Press esc to stop or any button to continue\n");
-            ch = _getch();
-        } else
-            printf("Please, follow instructions\n");
+        input_coeficients(&coeficients);
+        calculate_solutions(coeficients, &solutions);
+        print_solutions(solutions);
+        printf("Press esc to stop or any button to continue\n");
+        ch = _getch();
     } while (ch != esc_button);
 }
 
@@ -40,54 +40,50 @@ void greeting(void) {
     printf("Enter in a row, separated by spaces, the coefficients of the quadratic equation of the form: ax^2 + bx + c = 0\n");
 }
 
-bool check_input(void) {
-    if (getchar() != '\n') {
-        return false;
-    } else {
-        return true;
-    }
-}
-
-void input_coeficients(float *a, float *b, float *c) {
-    while (scanf("%f%f%f", a, b, c) != 3) {
+void input_coeficients(struct Coeficients *coeficients) {
+    while (scanf("%f%f%f", &coeficients->a, &coeficients->b, &coeficients->c) != 3) {
         while (getchar() != '\n')
             ;
         printf("Wrong input\n");
-        }
+    }
 }
 
-struct Solutions *calculate_solutions(struct Coeficients coeficients) {
+void calculate_solutions(struct Coeficients coeficients, struct Solutions *solutions) {
     float D = float(pow(coeficients.b, 2)) - 4 * coeficients.a * coeficients.c;
-    static struct Solutions solutions;
 
     if (fabs(coeficients.a) < EPSILON) {
-        if (fabs(coeficients.b) < FLT_EPSILON)
-            solutions.quantity = 3;
+        if (fabs(coeficients.b) < EPSILON)
+            solutions->quantity = 3;
         else {
-            solutions.quantity = 1;
-            solutions.solution_1 = -coeficients.c / coeficients.b;
+            solutions->quantity = 1;
+            solutions->solution_1 = -coeficients.c / coeficients.b;
         }
     } else {
-        if (fabs(D) < EPSILON){
-            solutions.quantity = 1;
-            solutions.solution_1 = -coeficients.b / (2 * coeficients.a);
-        } else if (D - EPSILON > 0) {
-            solutions.quantity = 2;
-            solutions.solution_1 = (-coeficients.b  + float(sqrt(D))) / (2 * coeficients.a);
-            solutions.solution_2 = (-coeficients.b  - float(sqrt(D))) / (2 * coeficients.a);
+        if (check_float_equality(D, 0)){
+            solutions->quantity = 1;
+            solutions->solution_1 = -coeficients.b / (2 * coeficients.a);
+        } else if (D > 0) {
+            solutions->quantity = 2;
+            solutions->solution_1 = (-coeficients.b  + float(sqrt(D))) / (2 * coeficients.a);
+            solutions->solution_2 = (-coeficients.b  - float(sqrt(D))) / (2 * coeficients.a);
         } else
-            solutions.quantity = 0;
+            solutions->quantity = 0;
     }
-    return &solutions;
 }
 
-void print_solutions(struct Solutions *solutions) {
-    if(fabs(solutions->quantity - 3) <= EPSILON)
+void print_solutions(struct Solutions solutions) {
+    if(solutions.quantity == 3)
         printf("infinity quantity of solutions\n");
-    else if(fabs(solutions->quantity) <= EPSILON)
+    else if(solutions.quantity == 0)
         printf("No solutions\n");
-    else if(fabs(solutions->quantity - 2) <= EPSILON)
-        printf("Solution_1 = %f\nSolution_2 = %f\n", solutions->solution_1, solutions->solution_2);
+    else if(solutions.quantity == 2)
+        printf("Solution_1 = %f\nSolution_2 = %f\n", solutions.solution_1, solutions.solution_2);
     else
-        printf("Solution_1 = %f\n", solutions->solution_1);
+        printf("Solution_1 = %f\n", solutions.solution_1);
+}
+
+bool check_float_equality(float f_1, float f_2)
+{
+    if(fabs(f_1 - f_2) <= EPSILON) return true;
+    else return false;
 }
