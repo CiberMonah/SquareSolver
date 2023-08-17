85ec5e622f2b3808045e43a5175cdd4b8f3f03c5
CiberMonah <142305833+CiberMonah@users.noreply.github.com>
2023-08-17 17:02:31 +0300

    Update SquareSolver.cpp

--- SquareSolver.cpp
+++ SquareSolver.cpp
@@ -2,31 +2,38 @@
 #include <math.h>
 #include <TXLib.h>
 
-float *calculate_solutions(float a, float b, float c);
-void print_solutions(float solutions[]);
+struct solutions {
+    int quantity;
+    float solution_1, solution_2;
+};
+
+struct coeficients {
+    float a, b, c;
+};
+
+struct solutions *calculate_solutions(struct coeficients coeficient);
+void print_solutions(struct solutions *solution);
 void greeting(void);
 bool check_input(void);
 void input_coeficients(float *a, float *b, float *c);
 
-float solutions[3] = {0, 0, 0};//solutions[quantity, 1_solution, 2_solution]
+const int esc_button = 27;
+const float EPSILON = float(0.00001);
 
 int main(void) {
-    float a, b, c;
+    struct coeficients coeficient;
     int ch;
 
     do {
         greeting();
-        input_coeficients(&a, &b, &c);//in order
+        input_coeficients(&coeficient.a, &coeficient.b, &coeficient.c);//in order
         if (check_input()) {
-                print_solutions(calculate_solutions(a, b, c));
-                printf("Press esc to stop or any button to continue\n");
-                ch = _getch();
-                solutions[0] = 0;
-                solutions[1] = 0;
-                solutions[2] = 0;
-         }
-        else return 0;
-    }while (ch!= 27);
+            print_solutions(calculate_solutions(coeficient));
+            printf("Press esc to stop or any button to continue\n");
+            ch = _getch();
+        } else
+            printf("Please, follow instructions\n");
+    } while (ch != esc_button);
 }
 
 void greeting(void) {
@@ -35,48 +42,52 @@ void greeting(void) {
 
 bool check_input(void) {
     if (getchar() != '\n') {
-        printf("Please, follow instructions\n");
         return false;
-    }
-    else {
+    } else {
         return true;
     }
 }
 
 void input_coeficients(float *a, float *b, float *c) {
     while (scanf("%f%f%f", a, b, c) != 3) {
-        while (getchar()!='\n')
+        while (getchar() != '\n')
             ;
         printf("Wrong input\n");
         }
 }
 
-float *calculate_solutions(float a, float b, float c) {
-    float D = float(pow(b, 2)) - 4 * a * c;
+struct solutions *calculate_solutions(struct coeficients coeficient) {
+    float D = float(pow(coeficient.b, 2)) - 4 * coeficient.a * coeficient.c;
+    static struct solutions solution;
 
-    if (fabs(a) < FLT_EPSILON) {
-        if (fabs(b) < FLT_EPSILON) solutions[0] = 3;
+    if (fabs(coeficient.a) < EPSILON) {
+        if (fabs(coeficient.b) < FLT_EPSILON)
+            solution.quantity = 3;
         else {
-            solutions[0] = 1;
-            solutions[1] = -c / b;
+            solution.quantity = 1;
+            solution.solution_1 = -coeficient.c / coeficient.b;
         }
+    } else {
+        if (fabs(D) < EPSILON){
+            solution.quantity = 1;
+            solution.solution_1 = -coeficient.b / (2 * coeficient.a);
+        } else if (D - EPSILON > 0) {
+            solution.quantity = 2;
+            solution.solution_1 = (-coeficient.b  + float(sqrt(D))) / (2 * coeficient.a);
+            solution.solution_2 = (-coeficient.b  - float(sqrt(D))) / (2 * coeficient.a);
+        } else
+            solution.quantity = 0;
     }
-    else {
-        if (fabs(D) < FLT_EPSILON){
-            solutions[0] = 1;
-            solutions[1] = -b / (2 * a);
-            }
-        if  (D - FLT_EPSILON > 0) {
-            solutions[0] = 2;
-            solutions[1] = (-b  + float(sqrt(D))) / (2 * a);
-            solutions[2] = (-b  - float(sqrt(D))) / (2 * a);
-        }
-    }
-    return solutions;
+    return &solution;
 }
 
-void print_solutions(float *sol) {
-    if(fabs(sol[0] - 3) <= FLT_EPSILON) printf("infinity quantity of solutions\n");
-    if(fabs(sol[0]) <= FLT_EPSILON) printf("No solutions\n");
-    for(int i = 1;i < int(sol[0]) + 1; i++) printf("Solution%d = %f\n", i, sol[i]);
+void print_solutions(struct solutions *solution) {
+    if(fabs(solution->quantity - 3) <= EPSILON)
+        printf("infinity quantity of solutions\n");
+    else if(fabs(solution->quantity) <= EPSILON)
+        printf("No solutions\n");
+    else if(fabs(solution->quantity - 2) <= EPSILON)
+        printf("Solution_1 = %f\nSolution_2 = %f\n", solution->solution_1, solution->solution_2);
+    else
+        printf("Solution_1 = %f\n", solution->solution_1);
 }
