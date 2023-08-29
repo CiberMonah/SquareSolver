3a3fd554c35d2d1e2be29804b487e3f7f7f03812
CiberMonah <142305833+CiberMonah@users.noreply.github.com>
2023-08-29 14:19:15 +0300

    Update Assert.h
    Addited codestyle

--- Assert.h
+++ Assert.h
@@ -6,11 +6,12 @@
 #ifdef NDEBUG
 #define Assert(condition) ((void)0)
 #else
-#define Assert(condition) ({ \
-if (!condition) {            \
-    printf("Assertion failed: in file: %s, in function: %s, in line: %d", __FILE__, __FUNCTION__, __LINE__);           \
-    abort();            \
-}         })
+#define Assert(condition) ({                                                                                            \
+    if (!condition) {                                                                                                   \
+        printf("Assertion failed: in file: %s, in function: %s, in line: %d", __FILE__, __FUNCTION__, __LINE__);        \
+        abort();                                                                                                        \
+    }                                                                                                                   \
+})
 #endif
 
 #endif // ASSERT_H_INCLUDED
